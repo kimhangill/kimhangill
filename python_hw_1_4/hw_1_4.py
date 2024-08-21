08a46ce8203ecfed6a2ea04e98bdde63b6130961
kimhangill <ytth918@naver.com>
2024-08-22 00:38:50 +0900

    Add files via upload
    7월 3~4째주 기초문

---
+++ python_hw_1_4/hw_1_4.py
@@ -0,0 +1,9 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+
+a = "사과는 영어로 apple "
+b = "바나나는 영어로 banna"
+c = "키위는 영어로 kiwi"
+
+print(f'{a} \n {b} \n {c}')
---
+++ python_ws_1_1/ws_1_1.py
@@ -0,0 +1,13 @@
+name = '홍길동'
+old = 20
+name_label= '이름 : '
+age = '나이 : '
+imformation = '학생 정보를 출력합니다.'
+example = '각각의 정보는 다음과 같습니다'
+pprint = '출력 완료'
+
+print(imformation)
+print(example)
+print(name_label, name)
+print(age, old)
+print(pprint)
